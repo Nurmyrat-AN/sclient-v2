beece4420c92ece58de84d0cc17433181e298c4f
Nurmyrat-AN <nurmyratnurmyrat457@gmail.com>
2023-05-08 22:12:48 +0500

    1

--- client/src/config/request.tsx
+++ client/src/config/request.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const _axios = axios.create({
-    // baseURL: '/api',
+    baseURL: '/api',
     // baseURL: 'http://localhost:3001/api',
-    baseURL: 'https://bonus.mebato-count.com/api',
+    // baseURL: 'https://bonus.mebato-count.com/api',
     withCredentials: true
 })
