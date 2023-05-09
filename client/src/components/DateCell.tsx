8f27136ff5a7a5549769f7b386e2dd692b17d0ab
Nurmyrat-AN <nurmyratnurmyrat457@gmail.com>
2023-05-09 21:42:35 +0500

    1

--- client/src/components/DateCell.tsx
+++ client/src/components/DateCell.tsx
@@ -6,7 +6,9 @@ import React from "react"
 export const DateCell = ({ query, homePage, colSpan = 1, onChange }: { query: any, homePage?: boolean, colSpan?: number, onChange?: (props: { date1: string, date2: string }) => void }) => {
     const startDateRef = React.useRef<HTMLInputElement>() as React.RefObject<HTMLInputElement>
     const endDateRef = React.useRef<HTMLInputElement>() as React.RefObject<HTMLInputElement>
-
+    const handleChange = () => {
+        console.log(startDateRef?.current?.value, startDateRef?.current?.value)
+    }
     return (
         <DateContainer onChange={({ date1, date2 }) => {
             if (startDateRef.current) startDateRef.current.value = date1
@@ -15,9 +17,9 @@ export const DateCell = ({ query, homePage, colSpan = 1, onChange }: { query: an
         }}>
             <TableCell colSpan={colSpan} align='center'>
                 <div style={{ display: 'flex', whiteSpace: 'nowrap', alignItems: 'center', justifyContent: 'center' }}>
-                    <TextField style={{ width: 140 }} size='small' inputRef={startDateRef} defaultValue={homePage ? undefined : query.startdate || today1} name='startdate' type='date' />
+                    <TextField onChange={handleChange} style={{ width: 140 }} size='small' inputRef={startDateRef} defaultValue={homePage ? undefined : query.startdate || today1} name='startdate' type='date' />
                     <Typography variant='button' style={{ margin: '0 4px' }}>-</Typography>
-                    <TextField style={{ width: 140 }} size='small' inputRef={endDateRef} defaultValue={homePage ? undefined : query.enddate || today2} name='enddate' type='date' />
+                    <TextField onChange={handleChange} style={{ width: 140 }} size='small' inputRef={endDateRef} defaultValue={homePage ? undefined : query.enddate || today2} name='enddate' type='date' />
                 </div>
             </TableCell>
         </DateContainer>
