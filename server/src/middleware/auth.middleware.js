199c4e6e1e3c394eb898338aac3a8d80f92f2ee6
Nurmyrat-AN <nurmyratnurmyrat457@gmail.com>
2023-05-10 11:27:32 +0500

    1

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -6,7 +6,13 @@ const authMiddleware = async (req, res, next) => {
         req.owner = req.cookies['pc_name']
         const mainPassword = await mSettings.findOne({ where: { _name: 'main-app-key' } })
         req.hasAccessToApp = mainPassword?._value === req.cookies['main-app-key']
-        if (req._parsedUrl.pathname.indexOf('/api/devices/') !== 0 && req._parsedUrl.pathname.indexOf('/api/getSendableMessages') !== 0 && req._parsedUrl.pathname.indexOf('/api') === 0 && !req.hasAccessToApp) throw CustomError.badRequest({ status: 403, message: 'Access denied!' })
+        if (
+            req._parsedUrl.pathname.indexOf('/api/devices/') !== 0 &&
+            req._parsedUrl.pathname.indexOf('/api/aish/') !== 0 &&
+            req._parsedUrl.pathname.indexOf('/api/getSendableMessages') !== 0 &&
+            req._parsedUrl.pathname.indexOf('/api') === 0 &&
+            !req.hasAccessToApp
+        ) throw CustomError.badRequest({ status: 403, message: 'Access denied!' })
         const password = await mSettings.findOne({ where: { _name: 'device-app-key' } })
         req.isAdmin = password?._value === req.cookies['device-app-key']
         next()
