4d6f2150f05fd6295761cd7653013fafbd4dd781
Nurmyrat-AN <nurmyratnurmyrat457@gmail.com>
2023-05-12 16:29:07 +0500

    1

--- server/src/db/config/index.js
+++ server/src/db/config/index.js
@@ -6,7 +6,8 @@ const sequelize = new Sequelize({
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    logging: false
+    logging: false,
+    retry: { timeout: 60, max: 100 }
 });
 
 
