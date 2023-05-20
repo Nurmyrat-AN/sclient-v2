801e98eef274036d3f0ca52dba99e85fbb6dcecd
Nurmyrat-AN <nurmyratnurmyrat457@gmail.com>
2023-05-20 21:49:19 +0500

    1

--- client/src/cpanel/pages/reports.tsx
+++ client/src/cpanel/pages/reports.tsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress, IconButton, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material"
+import { Avatar, Button, CircularProgress, IconButton, List, ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText, Table, TableBody, TableCell, TableHead, TableRow, TextField, Tooltip, Zoom } from "@mui/material"
 import { useNavigate, useSearchParams } from "react-router-dom"
 
 import { ACTION_TYPE_MODEL } from "../../types"
@@ -7,22 +7,32 @@ import { DateCell } from "../../components/DateCell"
 import { QueryListContainer } from "../../containers/querylist.container"
 import { SearchOutlined } from "@mui/icons-material"
 import moment from "moment"
+import React from "react"
+
+type DATA_TYPE = ACTION_TYPE_MODEL & {
+    count: number
+    sumBalance: number
+    sumAmount: number
+    grouppedActions: {
+        owner: string,
+        count: number,
+        sumBalance: number,
+        sumAmount: number
+    }[]
+}
+
+type FILTER_TYPE = {
+    name: string
+    offset: number
+    limit: number
+    startdate?: string
+    enddate?: string
+}
 
 export const ReportsPage = () => {
     const [query] = useSearchParams()
-    const navigate = useNavigate()
     return (
-        <QueryListContainer<ACTION_TYPE_MODEL & {
-            count: number
-            sumBalance: number
-            sumAmount: number
-        }, {
-            name: string
-            offset: number
-            limit: number
-            startdate?: string
-            enddate?: string
-        }>
+        <QueryListContainer<DATA_TYPE, FILTER_TYPE>
             initialFilter={{
                 name: query.get('name') || '',
                 offset: parseInt(query.get('offset') || '0'),
@@ -40,13 +50,7 @@ export const ReportsPage = () => {
                     {loading && <TableRow><TableCell align="center" colSpan={5}><CircularProgress size={20} /></TableCell></TableRow>}
                     {error && <TableRow><TableCell align="center" colSpan={5}><Button onClick={refresh} size='small'>{error}</Button></TableCell></TableRow>}
                     {!error && !loading && count === 0 && <TableRow><TableCell align="center" colSpan={5}>Tapylmady!</TableCell></TableRow>}
-                    {rows.map(data => <TableRow key={data.id}>
-                        <TableCell width={10}><IconButton onClick={() => navigate(`/cpanel/actions?actionTypeId=${data.id}&startdate=${filter.startdate}&enddate=${filter.enddate}`)}><SearchOutlined fontSize="small" /></IconButton></TableCell>
-                        <TableCell width={'100%'}>{data.name}</TableCell>
-                        <TableCell width={10} align="right" style={{ whiteSpace: 'nowrap' }}>{`${data.count || '0.00'} sany`}</TableCell>
-                        <TableCell width={10} align="right" style={{ whiteSpace: 'nowrap' }}>{`${data.sumAmount || '0.00'} TMT`}</TableCell>
-                        <TableCell width={10} align="right" style={{ whiteSpace: 'nowrap' }}>{`${data.sumBalance || '0.00'} TMT`}</TableCell>
-                    </TableRow>)}
+                    {rows.map(data => <ReportItem key={data.id} data={data} filter={filter} />)}
                 </TableBody>
                 <CustomTablePagination
                     count={count}
@@ -57,5 +61,31 @@ export const ReportsPage = () => {
                 />
             </Table>}
         />
+    )
+}
+
+const ReportItem = ({ data, filter }: { data: DATA_TYPE, filter: FILTER_TYPE }) => {
+    const [openTooltip, setOpenToolTip] = React.useState(false)
+    const navigate = useNavigate()
+    return (
+        <Tooltip open={openTooltip} onClose={() => setOpenToolTip(false)} followCursor TransitionComponent={Zoom} TransitionProps={{ timeout: 600 }} title={<List style={{ padding: 0 }}>
+            {data.grouppedActions.map(gAction => <ListItem key={gAction.owner}>
+                <ListItemIcon>
+                    <Avatar>{gAction.count || 0}</Avatar>
+                </ListItemIcon>
+                <ListItemText primary={gAction.owner} secondary={`${gAction.sumAmount || 0} TMT`} />
+                <ListItemSecondaryAction>
+                    {`${gAction.sumBalance || 0} TMT`}
+                </ListItemSecondaryAction>
+            </ListItem>)}
+        </List>}>
+            <TableRow onClick={() => setOpenToolTip(!openTooltip)}>
+                <TableCell width={10}><IconButton onClick={() => navigate(`/cpanel/actions?actionTypeId=${data.id}&startdate=${filter.startdate}&enddate=${filter.enddate}`)}><SearchOutlined fontSize="small" /></IconButton></TableCell>
+                <TableCell width={'100%'}>{data.name}</TableCell>
+                <TableCell width={10} align="right" style={{ whiteSpace: 'nowrap' }}>{`${data.count || '0.00'} sany`}</TableCell>
+                <TableCell width={10} align="right" style={{ whiteSpace: 'nowrap' }}>{`${data.sumAmount || '0.00'} TMT`}</TableCell>
+                <TableCell width={10} align="right" style={{ whiteSpace: 'nowrap' }}>{`${data.sumBalance || '0.00'} TMT`}</TableCell>
+            </TableRow>
+        </Tooltip>
     )
 }
